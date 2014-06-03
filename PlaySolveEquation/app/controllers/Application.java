d9458ee2c88bc5863376fbcee830309ba5100aca
EMAD <EMAD@3dns-2.adobe.com>
2014-06-03 12:18:48 +0300

    ResComm
    Signed-off-by: EMAD <EMAD@3dns-2.adobe.com>

---
+++ PlaySolveEquation/app/controllers/Application.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import play.data.Form;
+import play.data.DynamicForm;
+import play.mvc.*;
+import views.html.*;
+
+public class Application extends Controller {
+
+	private static double a;
+	private static String result = " . . . ";
+	private static double b;
+	private static double c;
+
+	public static Result index() {
+		return ok(index.render(result));
+	}
+
+	public static Result calc() {
+
+		DynamicForm form = Form.form().bindFromRequest();
+
+		a = Double.parseDouble(form.data().get("a"));
+		b = Double.parseDouble(form.data().get("b"));
+		c = Double.parseDouble(form.data().get("c"));
+
+		calcResult();
+
+		return redirect(routes.Application.index());
+	}
+
+	private static void calcResult() {
+
+		double delta = ((b * b) - (4 * a * c));
+
+		result = " x1 = " + (-b + Math.sqrt(delta)) / (2 * a) + " & "
+				+ " x2 = " + (-b - Math.sqrt(delta)) / (2 * a);
+
+	}
+}
---
+++ PlaySolveEquation/app/models/Solve.java
@@ -0,0 +1,20 @@
+package models;
+
+import java.util.*;
+
+public class Solve {
+    
+  public Long id;
+  public String label;
+  
+  public static List<Solve> all() {
+    return new ArrayList<Solve>();
+  }
+  
+  public static void create(Solve task) {
+  }
+  
+  public static void delete(Long id) {
+  }
+    
+}
